dd639a411b0039b123b203adad740aacab85c142
mutoulazy <mutoulazy@gmail.com>
2018-09-18 17:34:48 +0800

    使用gradle配置springcloud配置文件中心并完成测试 从github读取配置文件

---
+++ config-client/src/test/java/com/example/spring/cloud/weather/ConfigClientApplicationTest.java
@@ -0,0 +1,20 @@
+package com.example.spring.cloud.weather;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigClientApplicationTest {
+  @Value("${auther}")
+  private String auther;
+
+  @Test
+  public void contextLoads() {
+    Assert.assertEquals("waylau.com", auther);
+  }
+}
