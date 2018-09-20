6e0b3e87a2436507caea454087c3f4d07a9e2b81
mutoulazy <mutoulazy@gmail.com>
2018-09-20 16:07:54 +0800

    新增手动触发（由于没有部署到服务器不能自动）更新各个服务配置文件 流程： 修改gitbub 的配置文件 触发config server服务器自动更新配置文件 然后通过rabbitMQ发送到各个client 更新配置

--- config-client/src/main/java/com/example/spring/cloud/weather/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/spring/cloud/weather/ConfigClientApplication.java
@@ -2,12 +2,14 @@ package com.example.spring.cloud.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RemoteApplicationEventScan
 public class ConfigClientApplication {
+
   public static void main(String[] args) {
     SpringApplication.run(ConfigClientApplication.class, args);
   }
